198cce54230bdc781cfbffecae826237bb5c9cf9
daddinuz <daddinuz@gmail.com>
2017-03-17 17:09:52 +0100

    add clib support

---
+++ strdup.cmake
@@ -0,0 +1,17 @@
+set(ARCHIVE_NAME strdup)
+set(ARCHIVE_PATH deps/${ARCHIVE_NAME})
+
+#####
+# Dependencies
+###
+set(DEPENDENCIES "")
+
+#####
+# Archive
+###
+file(GLOB HEADER_FILES ${ARCHIVE_PATH}/*.h)
+file(GLOB SOURCE_FILES ${ARCHIVE_PATH}/*.c)
+add_library(${ARCHIVE_NAME} ${HEADER_FILES} ${SOURCE_FILES})
+target_link_libraries(${ARCHIVE_NAME} PRIVATE ${DEPENDENCIES})
+
+message("${ARCHIVE_NAME}: ${HEADER_FILES} ${SOURCE_FILES} included")
